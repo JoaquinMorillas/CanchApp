2fc5e73591c63854f6dcac395b44451013d2c046
joaquin <joaquimmorillasarce@gmail.com>
2026-08-29 23:48:32 -0300

    fixed Global exception class so the handlers returns the correct http status and a add a generic exeption handler

--- backend/src/main/java/com/joaquin/CanchApp/exception/GlobalException.java
+++ backend/src/main/java/com/joaquin/CanchApp/exception/GlobalException.java
@@ -6,42 +6,43 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @ControllerAdvice
 public class GlobalException {
 
     @ExceptionHandler(EmailAlreadyExistsExcepction.class)
     public ResponseEntity<String> handleEmailAlreadyExisitException(EmailAlreadyExistsExcepction ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
     @ExceptionHandler(IncorrectPasswordExcepion.class)
     public ResponseEntity<String> handleIncorrectPasswordExcepion(IncorrectPasswordExcepion ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
     }
     @ExceptionHandler(UserIdNotFoundException.class)
     public ResponseEntity<String> handlUserIdNotFoundException(UserIdNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
     
     @ExceptionHandler(UserEmailNotFoundException.class)
     public ResponseEntity<String> handleUserEmailNotFoundException(UserEmailNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(AddressAlreadyExistsException.class)
     public ResponseEntity<String> handleAddressAlreadyExistsException(AddressAlreadyExistsException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
     @ExceptionHandler(StablishmentNameAlreadyExistsException.class)
     public ResponseEntity<String> handleStablishmentNameAlreadyExistsException(StablishmentNameAlreadyExistsException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
     @ExceptionHandler(StablishmentIdNotFoundException.class)
     public ResponseEntity<String> handleStablishmentIdNotFoundException(StablishmentIdNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(OwnerHasNoStablishmentsException.class)
@@ -51,32 +52,32 @@ public class GlobalException {
 
     @ExceptionHandler(SportFieldNameAlreadyExistsException.class)
     public ResponseEntity<String> handleSportFieldNameAlreadyExistsException(SportFieldNameAlreadyExistsException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
     @ExceptionHandler(SportFieldIdNotFoundException.class)
     public ResponseEntity<String> handleSportFieldIdNotFoundException(SportFieldIdNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(PhotoUrlAlreadyExists.class)
     public ResponseEntity<String> handlePhotoUrlAlreadyExists(PhotoUrlAlreadyExists ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
     @ExceptionHandler(PhotoIdNotFoundException.class)
     public ResponseEntity<String> handlePhotoIdNotFoundException(PhotoIdNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(AvailabilityAlreadyExistsException.class)
     public ResponseEntity<String> handleAvailabilityAlreadyExistsException(AvailabilityAlreadyExistsException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
     @ExceptionHandler(AvailabilityIdNotFoundException.class)
     public ResponseEntity<String> handleAvailabilityIdNotFoundException(AvailabilityIdNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(DurationLenghtDifferentFromExpected.class)
@@ -86,12 +87,12 @@ public class GlobalException {
 
     @ExceptionHandler(ReservationBeginingHourNotAvailableException.class)
     public ResponseEntity<String> handleReservationBeginingHourNotAvailableException(ReservationBeginingHourNotAvailableException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
     @ExceptionHandler(ReservationIdNotFoundException.class)
     public ResponseEntity<String> handleReservationIdNotFoundException(ReservationIdNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(BeginingTimeIsAfterEndingTimeException.class)
@@ -101,7 +102,7 @@ public class GlobalException {
 
     @ExceptionHandler(TokenIsExipiredException.class)
     public ResponseEntity<String> handleTokenIsExipiredException(TokenIsExipiredException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
     }
 
     @ExceptionHandler(InvalidEmailException.class)
@@ -111,12 +112,12 @@ public class GlobalException {
 
     @ExceptionHandler(AmenityNameNotFoundException.class)
     public ResponseEntity<String> handleAmenityNameNotFoundException(AmenityNameNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(PolicyIdNotFoundException.class)
     public ResponseEntity<String> handlePolicyIdNotFoundException(PolicyIdNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(RatingMustHaveValueException.class)
@@ -126,17 +127,17 @@ public class GlobalException {
 
     @ExceptionHandler(SportNameNotFoundException.class)
     public ResponseEntity<String> handleSportNameNotFoundException(SportNameNotFoundException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(ReservationIsAlreadyConfirmedException.class)
     public ResponseEntity<String> handleReservationIsAlreadyConfirmedException(ReservationIsAlreadyConfirmedException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
 
     }
     @ExceptionHandler(ReservationUserIdIsDiferentFromTheIdSuppliedException.class)
     public ResponseEntity<String> handleReservationUserIdIsDiferentFromTheIdSuppliedException(ReservationUserIdIsDiferentFromTheIdSuppliedException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
     }
 
     @ExceptionHandler(ReservationUserIsNullException.class)
@@ -156,6 +157,12 @@ public class GlobalException {
 
     @ExceptionHandler(UserIsNotTheOwnerException.class)
     public ResponseEntity<String> handleUserIsNotTheOwnerException(UserIsNotTheOwnerException ex){
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneric(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("Ocurrió un error inesperado");
     }
 }
